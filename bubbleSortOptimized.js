9d52e1a190eff41a8885818922289010ab898638
Alexandra Herting <a-seda@onu.edu>
2022-03-17 11:33:29 -0400

    Add files via upload

---
+++ bubbleSortOptimized.js
@@ -0,0 +1,24 @@
+// Bubble Sort pseudocode
+// Start looping from with a variable called i the end of the array towards the beginning
+// Start an inner loop with a variable called j from the beginning until i-1
+// If arr[j] is greater than arr[j+1], swap those two values!
+// Return the sorted array
+
+// Optimized with noswaps variable
+function bubbleSort(arr) {
+  let noSwaps;
+  for (let i = arr.length; i > 0; i--) {
+    noSwaps = true;
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        // SWAP!
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        noSwaps = false;
+      }
+    }
+    if (noSwaps) break;
+  }
+  return arr;
+}
