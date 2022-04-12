e9ccd4f10bc74044d3855891a501b0703996d620
Alexandra Herting <a-seda@onu.edu>
2022-04-12 10:24:07 -0400

    Update queue.js

--- Data Structures/Queues/queue.js
+++ Data Structures/Queues/queue.js
@@ -14,7 +14,10 @@ class Queue {
     this.last = null;
     this.size = 0;
   }
-
+//Function accepts a value and creates a new node. 
+//If there is no node in the queue, set the first 
+//and last to the newNode, set this.last to new node. 
+//Increment queue size and return size.
   enqueue(val) {
     let newNode = new Node(val);
     if (!this.first) {
@@ -26,7 +29,12 @@ class Queue {
     }
     return ++this.size;
   }
-
+//If there are no nodes in the queue, return null, 
+//else create a temp variable to store the 1st property
+//on queue. If there is only 1 node, set the last property 
+//to be null. If there is more than one node, set the first
+//property to be the next property on the current first. 
+//Decrement size of stack, and return the removed node. 
   dequeue() {
     if (!this.first) return null;
     let temp = this.first;
