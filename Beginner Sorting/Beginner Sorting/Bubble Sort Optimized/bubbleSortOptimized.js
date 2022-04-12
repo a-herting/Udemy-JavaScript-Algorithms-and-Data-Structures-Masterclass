589bd95a5323be31ba06fecc63f88cb957e1b357
Alexandra Herting <a-seda@onu.edu>
2022-04-12 09:25:26 -0400

    Update bubbleSortOptimized.js

--- Beginner Sorting/Beginner Sorting/Bubble Sort Optimized/bubbleSortOptimized.js
+++ Beginner Sorting/Beginner Sorting/Bubble Sort Optimized/bubbleSortOptimized.js
@@ -1,5 +1,5 @@
 // Bubble Sort pseudocode
-// Start looping from with a variable called i the end of the array towards the beginning
+// Start looping with a variable called i set to arr.length and loop from the end of the array towards the beginning
 // Start an inner loop with a variable called j from the beginning until i-1
 // If arr[j] is greater than arr[j+1], swap those two values!
 // Return the sorted array
